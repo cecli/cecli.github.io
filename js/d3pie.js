141dfd12cc46a88ca30e2c9fc2389dfeece83e68
Thomas Juel Neergaard <khaine775@hotmail.com>
2017-05-07 19:00:27 +0200

    fixey

--- js/d3pie.js
+++ js/d3pie.js
@@ -1311,7 +1311,7 @@ var segments = {
 			.attr("transform", function() { return math.getPieTranslateCenter(pieCenter); })
 			.attr("class", pie.cssPrefix + "pieChart");
 
-		var arc = d3.svg.arc()
+		var arc = d3.arc()
 			.innerRadius(pie.innerRadius)
 			.outerRadius(pie.outerRadius)
 			.startAngle(0)
@@ -1343,7 +1343,7 @@ var segments = {
 			.style("stroke", segmentStroke)
 			.style("stroke-width", 1)
 			.transition()
-			.ease(d3.easeCubicInOut())
+			.ease(d3.easeCubicInOut)
 			.duration(loadSpeed)
 			.attr("data-index", function(d, i) { return i; })
 			.attrTween("d", function(b) {
